5c3e0ac2859bbbc352966c8650b365740672c8ed
James Watling <watling.james@gmail.com>
2016-09-27 11:52:24 +0800

    Initial commit of ListViewSelect

---
+++ ListViewSelect.js
@@ -0,0 +1,134 @@
+import React, { Component } from 'react';
+import {
+  ListView,
+  PropTypes,
+  StyleSheet,
+  Text,
+  Dimensions,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+const SCREEN_HEIGHT = Dimensions.get('window').height;
+const noop = () => {};
+const ds = new ListView.DataSource({rowHasChanged: (r1,r2)=>(r1!==r2)});
+
+export default class ListViewSelect extends Component {
+
+  static propTypes: {
+    list: PropTypes.array.isRequired,
+    isVisible: PropTypes.bool,
+    onClick: PropTypes.func,
+    onClose: PropTypes.func,
+  }
+
+  static defaultProps: {
+    list: [""],
+    isVisible: false,
+    onClick: noop,
+    onClose: noop
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataSource: ds.cloneWithRows(this.props.list),
+    };
+  }
+
+  componentWillReceiveProps(nextProps:any) {
+    if (nextProps.list !== this.props.list) {
+      this.setState({ dataSource: ds.cloneWithRows(nextProps.list) });
+    }
+  }
+
+  handleClick(data) {
+    this.props.onClick(data);
+    this.props.onClose();
+  }
+
+  renderRow(rowData) {
+    const separatorStyle = this.props.separatorStyle || DefaultStyles.separator;
+    const rowTextStyle = this.props.rowText || DefaultStyles.rowText;
+
+    let separator = <View style={separatorStyle} />;
+    if (rowData === this.props.list[0]) {
+      separator = null;
+    }
+
+    let row = <Text style={rowTextStyle}>{rowData}</Text>;
+    if (this.props.renderRow) {
+      row = this.props.renderRow(rowData);
+    }
+
+    return (
+      <View>
+        {separator}
+        <TouchableOpacity onPress={() => this.handleClick(rowData)}>
+          {row}
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
+  renderList() {
+    let maxHeight = {};
+    if (this.props.list.length > 12) {
+      maxHeight = { height: SCREEN_HEIGHT * 3 / 4 };
+    }
+    return (
+      <ListView
+        style={maxHeight}
+        dataSource={this.state.dataSource}
+        renderRow={(rowData) => this.renderRow(rowData)}
+        automaticallyAdjustContentInsets={false}
+      />
+    );
+  }
+
+  render() {
+    const containerStyle = this.props.containerStyle || DefaultStyles.container;
+    const popoverStyle = this.props.popoverStyle || DefaultStyles.popover;
+
+    if (this.props.isVisible) {
+      return (
+        <TouchableOpacity onPress={this.props.onClose}>
+          <View style={containerStyle}>
+            <View style={popoverStyle}>
+              {this.renderList()}
+            </View>
+          </View>
+        </TouchableOpacity>
+      );
+    }
+    return <View />;
+  }
+};
+
+
+const DefaultStyles = StyleSheet.create({
+  container: {
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    position: 'absolute',
+    justifyContent: 'center',
+    backgroundColor: 'transparent',
+  },
+  popover: {
+    margin: 10,
+    borderRadius: 3,
+    padding: 3,
+    backgroundColor: '#ffffff',
+  },
+  rowText: {
+    padding: 10,
+  },
+  separator: {
+    height: 0.5,
+    marginLeft: 8,
+    marginRight: 8,
+    backgroundColor: '#CCC',
+  },
+});
---
+++ README.md
@@ -0,0 +1,93 @@
+# React-Native List View Select
+
+## Installation instructions
+
+`npm install --save react-native-list-view-select`
+
+This module started as a personal fork of React Native List Popover homepage: https://github.com/bulenttastan/react-native-list-popover I have converted it to ES6 syntax and added support for React Native 0.24+
+
+List View Select is a designed to behave like a traditional `<select>` element in traditional HTML but with native components.
+
+The <ListViewSelect/> component allows you to add a very simple List view that is triggered over screen with a list of items as the child component and access the selected item from the parent component.
+
+The properties that this component accepts are
+* `list` Array of possible options
+* `isVisible` boolean to decide to show the Dropdown list or not
+* `onClick` callback function that takes an `item` parameter to handle the click operation
+* `onClose` callback function to set the isVisible to false to close the popover
+
+
+## Screenshots
+Here is a quick gif of it in use, excuse the resolution
+![](http://i.imgur.com/yzgYcNG.gif)
+# Before
+![](http://i.imgur.com/qK359Tm.png)
+# Selection
+![](http://i.imgur.com/niPxpss.png)
+# After Selection
+![](http://i.imgur.com/PVtsExi.png)
+
+
+# Usage
+
+```js
+import React, { Component } from 'react';
+import { StyleSheet, View, Text, TouchableHighlight } from 'react-native';
+import ListViewSelect from 'react-native-list-view-select';
+import _ from 'lodash';
+
+export default class ListViewSelectExample extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      item: "Select Item",
+      isVisible: false,
+    };
+    _.bindAll(this, ['showPopover', 'closePopover', 'setItem']);
+  }
+
+  showPopover() {
+    this.setState({isVisible: true});
+  }
+
+  closePopover() {
+    this.setState({isVisible: false});
+  }
+
+  setItem(item) {
+    this.setState({ item: item });
+  }
+
+  render() {
+    const { selectedCity } = this.props;
+    const items = [
+      "Item 1",
+      "Item 2",
+      "Item 3",
+      "Item 4",
+    ];
+
+    return (
+      <View style={styles.container}>
+        <TouchableHighlight onPress={this.showPopover}>
+          <Text>{this.state.item}</Text>
+        </TouchableHighlight>
+        <ListViewSelect
+          list={items}
+          isVisible={this.state.isVisible}
+          onClick={this.setItem}
+          onClose={this.closePopover}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 100,
+    paddingBottom: 100,
+  },
+});
+```
